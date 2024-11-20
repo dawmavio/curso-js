f019ef15c775364f8a226f2fa3871b49c9d5f345
Mavio Borges <dawmavio@gmail.com>
2024-11-20 21:56:53 +0100

    Agregando ejemplos de objetos

--- objetos/main.js
+++ objetos/main.js
@@ -1,3 +1,27 @@
 "use strict";
 
-let user
+class worker {
+    constructor(nombre,puesto,nivel,contrato){
+        this._nombre = nombre;
+        this._puesto = puesto;
+        this._nivel = nivel;
+        this._contrato = contrato;
+    }
+
+    messageAlertName(){
+        return alert(this._nombre);
+    }
+    messageAlerContrato(){
+        return alert(this._contrato);
+    }
+    messageAlertPuesto(){
+        return alert(this._puesto);
+    }
+    messageAlertNivel(){
+        return alert(this._nivel);
+    }
+}
+
+let worker1 = new worker("Mavio","Azafato",4,64);
+
+worker1.messageAlertNivel();
